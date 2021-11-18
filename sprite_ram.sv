dbf72c050aa675c119f895334165e69f896168eb
lclee <lclee3@illinois.edu>
2021-11-18 01:45:18 -0600

    all sprites are loaded, implemented sprite indexing scheme

--- sprite_ram.sv
+++ sprite_ram.sv
@@ -15,7 +15,7 @@ module  sprite_ram
 ADDR[16]    1 iff chess piece is white
 ADDR[15]    1 iff chess piece on light background
 ADDR[14:12] determines type of chess piece
-				0 for null
+				0 for empty
 				1 for pawn
 				2 for bishop
 				3 for knight
@@ -25,15 +25,57 @@ ADDR[14:12] determines type of chess piece
 
 ADDR[11:0]  pixel address in raster order from 0 to 3599 inclusive
 
+IMPORTANT: for each empty sprites, we load it twice for both white and dark so there is no confusion.
+
 ************************************************************************/
-logic [2:0] mem [`SPRITE_H_PIXEL * `SPRITE_V_PIXEL* `SPRITE_COUNT];
+
+logic [2:0] mem [131071];
 
 
 initial begin
-
-	 $readmemh("ece385sprites/text/piece_wk_background_l.txt", mem, 0, 3599);
-	 $readmemh("ece385sprites/text/piece_wp_background_d.txt", mem, 3600, 7199);
 	 
+	// Loading empty
+	$readmemh("ece385sprites/text/null_background_d.txt", mem, 17'b00000000000000000, 17'b00000111000001111);
+	$readmemh("ece385sprites/text/null_background_l.txt", mem, 17'b01000000000000000, 17'b01000111000001111);
+	$readmemh("ece385sprites/text/null_background_d.txt", mem, 17'b10000000000000000, 17'b10000111000001111);
+	$readmemh("ece385sprites/text/null_background_l.txt", mem, 17'b11000000000000000, 17'b11000111000001111);
+
+	// Loading pawn
+	$readmemh("ece385sprites/text/piece_bp_background_d.txt", mem, 17'b00001000000000000, 17'b00001111000001111);
+	$readmemh("ece385sprites/text/piece_bp_background_l.txt", mem, 17'b01001000000000000, 17'b01001111000001111);
+	$readmemh("ece385sprites/text/piece_wp_background_d.txt", mem, 17'b10001000000000000, 17'b10001111000001111);
+	$readmemh("ece385sprites/text/piece_wp_background_l.txt", mem, 17'b11001000000000000, 17'b11001111000001111);
+
+	// Loading bishop
+	$readmemh("ece385sprites/text/piece_bb_background_d.txt", mem, 17'b00010000000000000, 17'b00010111000001111);
+	$readmemh("ece385sprites/text/piece_bb_background_l.txt", mem, 17'b01010000000000000, 17'b01010111000001111);
+	$readmemh("ece385sprites/text/piece_wb_background_d.txt", mem, 17'b10010000000000000, 17'b10010111000001111);
+	$readmemh("ece385sprites/text/piece_wb_background_l.txt", mem, 17'b11010000000000000, 17'b11010111000001111);
+
+	// Loading knight
+	$readmemh("ece385sprites/text/piece_bn_background_d.txt", mem, 17'b00011000000000000, 17'b00011111000001111);
+	$readmemh("ece385sprites/text/piece_bn_background_l.txt", mem, 17'b01011000000000000, 17'b01011111000001111);
+	$readmemh("ece385sprites/text/piece_wn_background_d.txt", mem, 17'b10011000000000000, 17'b10011111000001111);
+	$readmemh("ece385sprites/text/piece_wn_background_l.txt", mem, 17'b11011000000000000, 17'b11011111000001111);
+
+	// Loading rook
+	$readmemh("ece385sprites/text/piece_br_background_d.txt", mem, 17'b00100000000000000, 17'b00100111000001111);
+	$readmemh("ece385sprites/text/piece_br_background_l.txt", mem, 17'b01100000000000000, 17'b01100111000001111);
+	$readmemh("ece385sprites/text/piece_wr_background_d.txt", mem, 17'b10100000000000000, 17'b10100111000001111);
+	$readmemh("ece385sprites/text/piece_wr_background_l.txt", mem, 17'b11100000000000000, 17'b11100111000001111);
+
+	// Loading queen
+	$readmemh("ece385sprites/text/piece_bq_background_d.txt", mem, 17'b00101000000000000, 17'b00101111000001111);
+	$readmemh("ece385sprites/text/piece_bq_background_l.txt", mem, 17'b01101000000000000, 17'b01101111000001111);
+	$readmemh("ece385sprites/text/piece_wq_background_d.txt", mem, 17'b10101000000000000, 17'b10101111000001111);
+	$readmemh("ece385sprites/text/piece_wq_background_l.txt", mem, 17'b11101000000000000, 17'b11101111000001111);
+	
+	// Loading king
+	$readmemh("ece385sprites/text/piece_bk_background_d.txt", mem, 17'b00110000000000000, 17'b00110111000001111);
+	$readmemh("ece385sprites/text/piece_bk_background_l.txt", mem, 17'b01110000000000000, 17'b01110111000001111);
+	$readmemh("ece385sprites/text/piece_wk_background_d.txt", mem, 17'b10110000000000000, 17'b10110111000001111);
+	$readmemh("ece385sprites/text/piece_wk_background_l.txt", mem, 17'b11110000000000000, 17'b11110111000001111);
+	
 end
 
 
